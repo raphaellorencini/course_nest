b346d1cec6c0bc612b6cc0aee9b6b91849d78605
Raphael Lorencini <raphael.lorencini@paytime.com.br>
2022-10-31 13:30:44 -0300

    Config for TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'course_nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoursesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
